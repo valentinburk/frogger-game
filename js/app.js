6c7c79fb1ed8c4d1ea1cc622554ef7d1535f9bee
Valentin Burkin <burkinman@me.com>
2018-03-31 12:10:43 +0300

    feat: Implement basic game functionality

--- js/app.js
+++ js/app.js
@@ -1,46 +1,149 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/** Class representing physical object body for collisions */
+class Rigidbody {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+  /**
+   * @description Checks if current body collides with other Rigidbody
+   * @param {Rigidbody} other Other body to check collision with
+   * @returns {bool} True if collides
+   */
+  collides(other) {
+    return !(
+      this.x + this.width < other.x ||
+      this.y + this.height < other.y ||
+      other.x + other.width < this.x ||
+      other.y + other.height < this.y
+    );
+  }
+}
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+/** Class representing a Player */
+class Player {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+    this.sprite = 'images/char-boy.png';
+    this.body = new Rigidbody(this.x + 15, this.y + 60, 70, 85);
+
+    this.reset();
+  }
+
+  update(dt) {
+    this.body.x = this.x + 15;
+    this.body.y = this.y + 60;
+  };
+
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+  handleInput(key) {
+    switch (key) {
+      case 'left':
+        this.x -= 100;
+        break;
+      case 'right':
+        this.x += 100;
+        break;
+      case 'up':
+        this.y -= 85;
+        break;
+      case 'down':
+        this.y += 85;
+        break;
+    }
 
+    this.fixOffscreen();
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+    if (this.waterReached()) {
+      this.reset();
+    }
+  }
 
+  fixOffscreen() {
+    if (this.x > 400) {
+      this.x = 400;
+    }
 
+    if (this.x < 0) {
+      this.x = 0;
+    }
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+    if (this.y > 400) {
+      this.y = 400;
+    }
+  }
+
+  waterReached() {
+    return this.y < 60;
+  }
+
+  reset() {
+    this.x = 200;
+    this.y = 400;
+  }
+}
+
+/** Class representing an Enemy */
+class Enemy {
+  constructor() {
+    this.x = -100;
+    this.y = 50 + Math.floor(Math.random() * 3) * 85;
+    this.speed = 100 + Math.floor(Math.random() * 100);
+
+    this.sprite = 'images/enemy-bug.png';
+    this.body = new Rigidbody(this.x, this.y + 75, 100, 70);
+  }
+
+  update(dt) {
+    this.x += dt * this.speed;
+
+    if (this.x > 505) {
+      this.reset();
+    }
+
+    // Update collision body
+    this.body.x = this.x;
+    this.body.y = this.y + 75;
+
+    // rect to the collision
+    if (this.body.collides(player.body)) {
+      player.reset();
+    }
+  };
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+
+  reset() {
+    this.x = -100;
+    this.speed = 100 + Math.floor(Math.random() * 100);
+  }
+}
+
+// Instantiate player
+const player = new Player();
+
+// Instantiate enemies
+const allEnemies = new Array();
+for (let i = 0; i < 4; i++) {
+  allEnemies.push(new Enemy());
+}
+
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
